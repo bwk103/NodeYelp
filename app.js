7c70178e97af1af9abf5ca470dcd11727ec7257c
Ben Kielty <bwk103@gmail.com>
2017-04-04 18:06:29 +0100

    Add bootstrap, creating form for post route

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+var bodyParser = require('body-parser');
 var campgrounds = [
-                  {name: "Salmon Creek", image: 'https://cspafford.files.wordpress.com/2011/04/salmon-creek-1.jpg'},
-                  {name: "Epsom Place", image: 'https://media.pitchup.co.uk/images/4/image/private/s--e0MM3PST--/c_fill,h_600,w_800/e_improve,fl_progressive/q_jpegmini/v1475496871/lee-valley-camping-and-caravan-park/x188855.jpg.pagespeed.ic.7TSawu-8t1.jpg'},
-                  {name: "Another Camp", image: 'http://1.bp.blogspot.com/-Kdiea2X41mM/Tf4PdkiFKiI/AAAAAAAAAOo/eMzfWeUBjK8/s1600/boules251295_10150212499241884_632581883_7730056_1508512_n.jpg'}
+                  {name: "Salmon Creek", image: 'https://farm4.staticflickr.com/3270/2617191414_c5d8a25a94.jpg'},
+                  {name: "Epsom Place", image: 'https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg'},
+                  {name: "Another Camp", image: 'https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg'}
                   ];
 
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true }));
 
 app.get("/", function(req, res){
   res.render("home");
@@ -15,6 +18,17 @@ app.get("/", function(req, res){
 
 app.get("/campgrounds", function(req, res){
   res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new.ejs");
+});
+
+app.post("/campgrounds", function(req, res){
+  var name = req.body.name;
+  var image = req.body.image;
+  campgrounds.push({name: name, image: image});
+  res.redirect("/campgrounds");
 });
 
 
