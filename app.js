e668269294b3ff3f7adb6f8b45b582a8216027a4
Ben Kielty <bwk103@gmail.com>
2017-04-05 16:23:21 +0100

    Create SHOW route
    Added campgrounds/show route.  Added description field to campground.
    Added integration with MongoDB.

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-var express = require('express');
-var app = express();
-var request = require('request');
-var bodyParser = require('body-parser');
-var campgrounds = [
-                  {name: "Salmon Creek", image: 'https://farm4.staticflickr.com/3270/2617191414_c5d8a25a94.jpg'},
-                  {name: "Epsom Place", image: 'https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg'},
-                  {name: "Another Camp", image: 'https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg'}
-                  ];
+var express     = require('express'),
+    app         = express(),
+    request     = require('request'),
+    bodyParser  = require('body-parser'),
+    mongoose    = require('mongoose');
+
+mongoose.connect('mongodb://localhost/yelp_camp');
 
 app.set('view engine', 'ejs');
+
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create({
+//     name: "Epsom Place",
+//     image: 'https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg',
+//     description: "A lovely place in the middle of Surrey. No beach though!"
+//   }, function(err, campground){
+//   if (err){
+//     console.log("Oh no there has been an error");
+//   } else {
+//     console.log("The campground has been saved to the database");
+//     console.log(campground);
+//   }
+// });
 
 app.use(bodyParser.urlencoded({extended: true }));
 
@@ -17,17 +36,44 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-  res.render("campgrounds", {campgrounds: campgrounds});
+  Campground.find({}, function(err, campgrounds){
+    if (err) {
+      console.log("There was a problem getting the campgrounds");
+    } else {
+      res.render("index", {campgrounds: campgrounds});
+    }
+  });
 });
 
 app.get("/campgrounds/new", function(req, res){
   res.render("new.ejs");
 });
 
+app.get("/campgrounds/:id", function(req, res){
+  Campground.findById(req.params.id, function(err, foundCampground){
+    if (err) {
+      console.log("There has been an error");
+    } else {
+      res.render("show", {campground: foundCampground});
+    }
+  });
+});
+
 app.post("/campgrounds", function(req, res){
   var name = req.body.name;
   var image = req.body.image;
-  campgrounds.push({name: name, image: image});
+  var description = req.body.description;
+  Campground.create({
+    name: name,
+    image: image,
+    description: description
+  }, function(err, campground){
+      if (err) {
+        console.log("There was a problem with your request");
+      } else {
+        console.log("Your campground was saved to the database");
+      }
+    });
   res.redirect("/campgrounds");
 });
 
