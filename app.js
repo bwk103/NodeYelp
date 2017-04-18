fc2ab31965ae07360ff15acc0433ae49fbbd6f4b
Ben Kielty <bwk103@gmail.com>
2017-04-18 15:05:36 +0100

    Add style to show route
    Adding stylesheets to show route.

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true }));
 
+app.use(express.static(__dirname + "/public"));
+
 seedDB();
 
 app.get("/", function(req, res){
